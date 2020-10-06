1449e208cfbb51c3e939cf1b66927115dc35fc9a
Mixergi <41150598+Mixergi@users.noreply.github.com>
2020-10-06 20:28:22 +0900

    #16 Update dataset

--- dataset.py
+++ dataset.py
@@ -1,22 +1,34 @@
 import os
+import random
 
 import numpy as np
 import tensorflow as tf
 
+class MelGAN_Dataset:
+    def __init__(self, data_dir, shuffle=True):
+        self.data_dir = data_dir
+        self.shuffle = shuffle
 
-def data_generator(data_dir):
-    for file_dir in os.listdir(data_dir):
-        file_dir = os.path.join(data_dir, file_dir)
-        mels, wav = np.load(file_dir, allow_pickle=True)
+    def _generator(self):
 
-        wav = np.expand_dims(wav, -1)
+        file_list = os.listdir(self.data_dir)
 
-        return mels, wav
+        if self.shuffle:
+            random.shuffle(file_list)
 
+        for file_dir in file_list:
+            file_dir = os.path.join(self.data_dir, file_dir)
+            mels, wav = np.load(file_dir, allow_pickle=True)
 
-def create_dataset(data_dir):
-    return tf.data.Dataset.from_generator(
-        data_generator,
-        output_types=(tf.float32, tf.float32),
-        args=data_dir
-    ).prefetch(tf.data.experimental.AUTOTUNE).batch(1)
+            wav = np.expand_dims(wav, -1)
+
+            return mels, wav
+
+    def create_dataset(self):
+        return tf.data.Dataset.from_generator(
+            self._generator, 
+            output_types=(tf.float32, tf.float32)
+            ).prefetch(tf.data.experimental.AUTOTUNE).batch(1)
+
+    def get_lengh(self):
+        return len(os.listdir(self.data_dir))
