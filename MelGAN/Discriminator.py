14da4f9aeff69cf4bf891be93cd22c4e8c48aef4
Mixergi <41150598+Mixergi@users.noreply.github.com>
2020-10-05 14:38:32 -1000

    #8 Add Discriminator Model

--- MelGAN/Discriminator.py
+++ MelGAN/Discriminator.py
@@ -5,13 +5,50 @@ class Discriminator_Block(tf.keras.layers.Layer):
     def __init__(self, **kwargs):
         super(Discriminator_Block, self).__init__(**kwargs)
 
+        down_sampling = [4, 4, 4, 4]
+        filters = [64, 256, 1024, 1024]
+
+        self.blocks = [
+            tf.keras.layers.Conv1D(16, 15, 1, padding='same'),
+            tf.keras.layers.LeakyReLU()
+        ]
+
+        for i, (d, f) in enumerate(down_sampling, filters):
+            self.blocks += [
+                tf.keras.layers.Conv1D(f, 41, d, padding='same', groups=d**(i+1)),
+                tf.keras.layers.LeakyReLU()
+            ]
+
+        self.blocks +=[
+            tf.keras.layers.Conv1D(1024, 5, 1, padding='same'),
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Conv1D(1, 3, 1, padding='same')
+        ]
+
     def call(self, x):
-        pass
+        outputs = []
+        for i, block in enumerate(self.blocks):
+            x = block(x)
+            if i % 2 == 0:
+                outputs.append(x)
+
+        return outputs
+        
 
 
 class Discriminator(tf.keras.models.Model):
-    def __init__(self, **kwargs):
+    def __init__(self, n_scale=3, **kwargs):
         super(Discriminator, self).__init__(**kwargs)
 
+        self.discriminator_blocks = [Discriminator_Block() for i in range(n_scale)]
+
+        self.avgpooling = tf.keras.layers.AveragePooling1D(4)
+
     def call(self, x):
-        pass
+        outputs = []
+        for block in self.discriminator_blocks:
+            x = block(x)
+            outputs.append(x)
+            x = self.avgpooling(x)
+        
+        return outputs
---
+++ MelGAN/trainer.py
@@ -0,0 +1,10 @@
+import math
+import os
+import time
+
+import numpy as np
+import tensorflow as tf
+import tqdm
+
+from Discriminator import Discriminator
+from Generator import Generator
