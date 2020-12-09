1f6d274825cc7c10404f17228203dcff98d076a3
Mixergi <41150598+Mixergi@users.noreply.github.com>
2020-12-10 08:49:58 +0900

    #29 Change dircetory

--- melgan/weight_layer.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-from torch.nn.utils import weight_norm
-
-
-def WMConv1d(*args, **kwargs):
-    return weight_norm(nn.Conv1d(*args, **kwargs))
-
-def WMConvTranspose1d(*args, **kwargs):
-    return weight_norm(nn.ConvTranspose1d(*args, **kwargs))
